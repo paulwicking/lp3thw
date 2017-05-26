5d1eac73eb191fe341e503ffdedbbba52b8e8b08
Paul Wicking <wowsuchnamaste@protonmail.com>
2017-05-26 12:15:07 +0200

    adding ex40 and oop test

---
+++ ex40.py
@@ -0,0 +1,20 @@
+class Song(object):
+	def __init__(self, lyrics):
+		self.lyrics = lyrics
+
+	def sing_me_a_song(self):
+		for line in self.lyrics:
+			print(line)
+
+bday_lyrics = """Happy birthday to you
+I don't want to get sued
+So I'll stop right there"""
+
+bulls_lyrics = """They rally around tha family
+With pockets full of shells"""
+
+happy_bday = Song([bday_lyrics])
+bulls_on_parade = Song([bulls_lyrics])
+
+happy_bday.sing_me_a_song()
+bulls_on_parade.sing_me_a_song()
