ead7faffbc2b0b7d0001a5efd161fe038328363d
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2017-05-28 22:48:34 +0200

    Adding unit tests for ex49, one missing.

--- ex40-49/ex49/tests/parser_tests.py
+++ ex40-49/ex49/tests/parser_tests.py
@@ -1,30 +1,42 @@
 from nose.tools import *
 from ex49.parser import * 
+from ex49.lexicon import *
 
 def test_peek():
-    pass
+    result = scan('north')
+    assert_equal(peek(result), 'direction')
 
 def test_match():
-    pass
+    assert_equal(match([('noun', 'player')], 'noun'), ('noun', 'player'))
 
 def test_skip():
-    # scream at the bear should return "scream" and "bear"
-    pass
+    result = scan('north')
+    assert_equal(result, [('direction', 'north')])
 
 def test_parse_verb():
-    #assert_raises "Expected a verb next."
-    pass
+    with assert_raises_regexp(ParserError, "Expected a verb next."):
+        parse_verb(['noun', 'player'])
 
 def test_parse_object():
-    # assert_raises "Expected a noun or direction next."
-    pass
+    with assert_raises_regexp(ParserError, 
+            "Expected a noun or direction next."):
+        parse_object([('error', 'IAS')])
 
 def test_parse_subject():
-    # assert_raises "Expected a verb next."
-    pass
+    assert_equal(parse_subject([('verb', 'run')]), ('noun', 'player'))
+    with assert_raises_regexp(ParserError, "Expected a verb next."): 
+        parse_subject(['noun', 'player'])
 
 def test_parse_sentence():
     pass
+"""
+    # TODO: Fix this test.
+    assert_equal(parse_sentence([('verb', 'run')]), [('verb', 'run')])
+    test_sentence = parse_sentence([('verb', 'run'),
+            ('direction', 'north'),
+            ('error', 'IAS')])
+    assert_equal(test_sentence, [('verb', 'run'),
+        ('direction', 'north'),
+        ('error', 'IAS')])
+"""
 
-def test_parse_sentence():
-    pass
