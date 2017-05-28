50c7f3ef30a3b88fa6eee2dec9877283efcdae5b
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2017-05-28 16:22:44 +0200

    adding ex49, skeleton tests only

---
+++ ex40-49/ex49/tests/parser_tests.py
@@ -0,0 +1,30 @@
+from nose.tools import *
+from ex49.parser import * 
+
+def test_peek():
+    pass
+
+def test_match():
+    pass
+
+def test_skip():
+    # scream at the bear should return "scream" and "bear"
+    pass
+
+def test_parse_verb():
+    #assert_raises "Expected a verb next."
+    pass
+
+def test_parse_object():
+    # assert_raises "Expected a noun or direction next."
+    pass
+
+def test_parse_subject():
+    # assert_raises "Expected a verb next."
+    pass
+
+def test_parse_sentence():
+    pass
+
+def test_parse_sentence():
+    pass
