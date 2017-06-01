f64490e13fb99b3f4f4af6c093ed2941aa2194c2
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2017-06-01 07:45:20 +0200

    Refactoring scan() in lexicon.py.
    * Removed function that checked if string is digit, replaced with built-in
    function 'isdigit()'.
    * Removed 'result' variable assignment, using just into function call.
    * Added comment in code.

--- ex40-49/ex49/ex49/lexicon.py
+++ ex40-49/ex49/ex49/lexicon.py
@@ -1,29 +1,22 @@
 def scan(words_to_scan):
     """
-    Scan words in lists, return a tuple with word category if found
-    rtype: tuple
+    Scan words in lists, returns a tuple (word:word category) if found
+    rtype: list of tuple
     """
     words = words_to_scan.split()
     word_pairs = []
 
+    # Words in the dictionary has to be lowercase, as the check depends on that.
     dictionary = {'north': 'direction', 'south': 'direction', 'east': 'direction', 'west': 'direction',
                   'go': 'verb', 'stop': 'verb', 'kill': 'verb', 'eat': 'verb',
                   'the': 'stop', 'in': 'stop', 'of': 'stop', 'from': 'stop', 'at': 'stop', 'it': 'stop',
                   'door': 'noun', 'bear': 'noun', 'princess': 'noun', 'cabinet': 'noun'}
 
-    def convert_to_number(string):
-        try:
-            return int(string)
-        except ValueError:
-            return None
-
     for word in words:
-        if not convert_to_number(word):
-            result = dictionary.get(word.lower(), 'error')
-            # yield word_pairs((result, word))
-            word_pairs.append((result, word))
-        else:
-            # yield(('number', word))
+        if word.isdigit():
             word_pairs.append(('number', word))
+
+        else:
+            word_pairs.append((dictionary.get(word.lower(), 'error'), word))
 
     return word_pairs
